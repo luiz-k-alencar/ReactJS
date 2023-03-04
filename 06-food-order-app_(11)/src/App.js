791ff98dd5ec08d7aba819373e56f41ec6c23843
Luiz Felipe <68920578+luiz-k-alencar@users.noreply.github.com>
2023-03-04 16:01:39 -0300

    Finishing project section 11

--- 06-food-order-app_(11)/src/App.js
+++ 06-food-order-app_(11)/src/App.js
@@ -1,8 +1,29 @@
+import { useState } from "react";
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCardHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCardHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCardHandler}/>}
+      <Header onShowCart={showCardHandler}/>
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
---
+++ 06-food-order-app_(11)/src/components/UI/Modal.js
@@ -0,0 +1,28 @@
+import { createPortal } from "react-dom";
+
+import styles from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onClose}/>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={styles.modal} on>
+      <div className={styles.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+  return (
+    <>
+      {createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
+      {createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+    </>
+  );
+};
+
+export default Modal;
